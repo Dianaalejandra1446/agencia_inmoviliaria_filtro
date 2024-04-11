d0096e1548a11450f81ce3f4c89e71ef20d920cd
Dianaalejandra1446 <dianaleja04@hotmail.com>
2024-04-11 16:00:38 -0500

    First Commit

---
+++ src/main/java/com/campuslands/agencia_inmoviliaria/AgenciaInmoviliariaApplication.java
@@ -0,0 +1,13 @@
+package com.campuslands.agencia_inmoviliaria;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AgenciaInmoviliariaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AgenciaInmoviliariaApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/campuslands/agencia_inmoviliaria/AgenciaInmoviliariaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.campuslands.agencia_inmoviliaria;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgenciaInmoviliariaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
